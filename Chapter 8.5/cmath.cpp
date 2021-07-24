ea68b4d96c024f0a8fba59c489bffe723107b244
Henry Lin <linhenry2004@gmail.com>
2021-07-24 13:38:21 +0800

    cmath C++

---
+++ Chapter 8.5/cmath.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+}
+
+/*
+sin(radians)
+cos(radians)
+tan(radians)
+asin(a)
+acos(a)
+atan(a)
+
+exp(x) -> e to the x
+log(x) -> log e (x)
+log10(x)
+pow(a, b)
+sqrt(x)
+
+四捨五入：
+ceil(x)
+floor(x)
+
+min(a, b) -> Returns the smaller number
+max(a, b) -> Returns the larger number
+abs(x)
+*/
