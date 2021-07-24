f04143572464fc7bf20d593f47a95008418535d4
Henry Lin <linhenry2004@gmail.com>
2021-07-25 01:49:00 +0800

    const function

---
+++ Objects and Classes/const function/CircleWithConstantMemberFunctions.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+    public: 
+        Circle();
+        Circle(double);
+        double getArea() const;
+        double getRadius() const;
+        void setRadius(double);
+        static int getNumberOfObjects();
+    private: 
+        double radius;
+        static int numberOfObjects;
+};
+
+int Circle::numberOfObjects = 0;
+
+Circle::Circle() {
+    radius = 1;
+    numberOfObjects++;
+}
+
+Circle::Circle(double newRadius) {
+    radius = newRadius;
+    numberOfObjects++;
+}
+
+//By adding the const method, it tells the program that this function is not supposed to change any variables. 
+//As a result, if it tries to change any variables, any error will occur. 
+double Circle::getArea() const{
+    return radius * radius * 3.14159;
+}
+
+double Circle::getRadius() const{
+    return radius;
+}
+
+void Circle::setRadius(double newRadius) {
+    radius = (newRadius >= 0) ? newRadius : 0;
+}
+
+int Circle::getNumberOfObjects() {
+    return numberOfObjects;
+}
+
+int main() {
+    cout << "Number of circle objects created: " << Circle::getNumberOfObjects() << endl;
+
+    Circle circle1;
+    cout << "The area of the circle of radius " << circle1.getRadius() << " is " << circle1.getArea()  << endl;
+    cout << "Number of circle objects created: " << Circle::getNumberOfObjects() << endl;
+
+    Circle circle2(5.0);
+    cout << "The area of the circle of radius " << circle2.getRadius() << " is " << circle2.getArea()  << endl;
+    cout << "Number of circle objects created: " << Circle::getNumberOfObjects() << endl;
+    
+    circle1.setRadius(3.3);
+    cout << "The area of the circle of radius " << circle1.getRadius() << " is " << circle1.getArea()  << endl;
+    cout << "circle1.getNumberOfObjects() returns " << circle1.getNumberOfObjects() << endl;
+    cout << "circle2.getNumberOfObjects() returns " << circle2.getNumberOfObjects() << endl;
+    
+    return 0;
+}
