68d537d0f0b5e5fcf79f3dd2bdfe40d09c12b576
Henry Lin <linhenry2004@gmail.com>
2022-02-13 17:28:11 +0800

    Update struct

--- Chapter 10/Struct_and_Pointer.cpp
+++ Chapter 10/Struct_and_Pointer.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-typedef struct vector {
+typedef struct vector
+{
     double x;
     double y;
     double z;
 } V;
-void enter(V*);
-void vput(V*, V*, char);
-V vadd(V*, V*);
-double vdot(V*, V*);
-V vcross(V*, V*);
+void enter(V *);
+void vput(V *, V *, char);
+V vadd(V *, V *);
+double vdot(V *, V *);
+V vcross(V *, V *);
 
-int main(void) {
+int main(void)
+{
     double d;
     V a, b, v;
     enter(&a);
@@ -30,28 +32,32 @@ int main(void) {
 
     return 0;
 }
-void vput(V* a, V* b, char ch) {
+void vput(V *a, V *b, char ch)
+{
     cout << "(" << a->x << "," << a->y << "," << a->z << ") " << ch << " (" << b->x << "," << b->y << "," << b->z << ") = ";
 }
-void enter(V* v) {
+void enter(V *v)
+{
     cout << "Input a vector (x, y, z): ";
-    cin >> v->x>>v->y>>v->z;
+    cin >> v->x >> v->y >> v->z; //The -> operator gives the value held by variable_name to structure or union variable pointer_name.
 }
-V vadd(V* a, V* b) {
+V vadd(V *a, V *b)
+{
     V v;
     v.x = a->x + b->x;
     v.y = a->y + b->y;
     v.z = a->z + b->z;
     return v;
 }
-double vdot(V* a, V* b) {
-    return a->x*b->x + a->y*b->y + a->z*b->z;
+double vdot(V *a, V *b)
+{
+    return a->x * b->x + a->y * b->y + a->z * b->z;
 }
-V vcross(V* a, V* b) {
+V vcross(V *a, V *b)
+{
     V v;
-    v.x = a->y*b->z - b->y*a->z;
-    v.y = a->z*b->x - b->z*a->x;
-    v.z = a->x*b->y - b->x*a->y;
+    v.x = a->y * b->z - b->y * a->z;
+    v.y = a->z * b->x - b->z * a->x;
+    v.z = a->x * b->y - b->x * a->y;
     return v;
 }
-//The -> operator gives the value held by variable_name to structure or union variable pointer_name.
