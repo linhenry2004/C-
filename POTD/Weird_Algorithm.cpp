87dcea9ba37afa295b059dd81ffcba712a2f586a
Henry Lin <linhenry2004@gmail.com>
2022-03-01 22:36:12 +0800

    POTD

---
+++ POTD/Weird_Algorithm.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int algo(int num)
+{
+    if (num == 1)
+    {
+        cout << num << endl;
+        return 0;
+    }
+    else
+    {
+        cout << num << " -> ";
+    }
+
+    if (num % 2 == 0)
+    {
+        return algo(num / 2);
+    }
+    else
+    {
+        return algo(num * 3 + 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Please insert a number: ";
+    cin >> n;
+    algo(n);
+    return 0;
+}
