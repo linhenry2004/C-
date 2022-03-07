1abf9fcabfd0eb1fd0a057a0bfb6db2470fa6ab3
Henry Lin <linhenry2004@gmail.com>
2022-03-07 11:50:50 +0800

    potd

---
+++ POTD/Fortune_Telling.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/1634/problem/B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numCases;
+    long a, sum, ans;
+    cin >> numCases;
+    string respond[numCases];
+    for (int i = 0; i < numCases; i++)
+    {
+        cin >> a >> sum >> ans;
+        long x;
+        for (int j = 0; j < a; j++)
+        {
+            cin >> x;
+            sum += x;
+        }
+        bool same = (ans % 2 == sum % 2);
+        if (same)
+        {
+            respond[i] = "Alice";
+        }
+        else
+        {
+            respond[i] = "Bob";
+        }
+    }
+    for (int i = 0; i < numCases; i++)
+    {
+        cout << respond[i] << endl;
+    }
+    return 0;
+}
