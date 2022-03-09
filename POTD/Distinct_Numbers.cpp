3184785ed6ddfd0c2934fac938b03515fda230ea
Henry Lin <linhenry2004@gmail.com>
2022-03-09 11:14:02 +0800

    POTD

--- POTD/Distinct_Numbers.cpp
+++ POTD/Distinct_Numbers.cpp
@@ -1,38 +1,24 @@
+#include <algorithm>
 #include <iostream>
 #include <list>
 using namespace std;
-
-void sort(long long arr[], int size)
-{
-    for (int j = 1; j < size; j++)
-    {
-        long long temp = arr[j];
-        int index = j;
-        while (index > 0 && temp < arr[index - 1])
-        {
-            arr[index] = arr[index - 1];
-            index--;
-        }
-        arr[index] = temp;
-    }
-}
 
 int main()
 {
     int n;
     cin >> n;
-    long long array[n];
+    long long arr[n];
     list<long long> result;
     for (int i = 0; i < n; i++)
     {
-        cin >> array[i];
+        cin >> arr[i];
     }
-    sort(array, n);
+    sort(arr, arr + n);
     for (int i = 0; i < n; i++)
     {
-        if (result.back() != array[i])
+        if (result.back() != arr[i])
         {
-            result.push_back(array[i]);
+            result.push_back(arr[i]);
         }
     }
     cout << result.size() << endl;
