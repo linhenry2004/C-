5d7edc082b43dc4154e4c821aa0c3c7b6bcb97ee
Henry Lin <linhenry2004@gmail.com>
2022-03-12 20:04:15 +0800

    POTD

--- POTD/Weird_Sort.cpp
+++ POTD/Weird_Sort.cpp
@@ -1,22 +1,32 @@
-#include <queue>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+
     int n;
     cin >> n;
-    priority_queue<long long> pq;
-    for (int i = 0; i < n; i++)
-    {
-        long long temp;
-        cin >> temp;
-        pq.push(temp);
+    vector<int> v(n);
+    for (auto &it: v) cin>>it;
+    // for (int x: v) cout << x << " ";
+    // cout << endl;
+
+    const int maxa = 100;
+    vector<int> cnt(maxa, 0);
+    for (auto it: v) cnt[it]++;
+    // for (int y: cnt) cout << y << " ";
+    // cout << endl;
+
+    vector<int> ans(n);
+    int id = 0;
+    for (int i = 0; i < maxa; i++) {
+        for (int j = 0; j < cnt[i]; j++) {
+            ans[id] = i;
+            id++;
+        }
     }
-    while (!pq.empty())
-    {
-        long long now = pq.top();
-        pq.pop();
-        cout << now << " ";
-    }
+    for (auto it: ans) cout << it << ' ';
+    cout << endl;
+
+    return 0;
 }
