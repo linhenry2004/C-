5791e14f9b214de6905c80fb946a35ca70387914
Henry Lin <linhenry2004@gmail.com>
2022-03-25 13:49:58 +0800

    POTD

--- POTD/Dice_Combinations.cpp
+++ POTD/Dice_Combinations.cpp
@@ -1,6 +1,20 @@
+#include <vector>
 #include <iostream>
 using namespace std;
+const int mod = 1e9+7;
 
 int main () {
-    
+    int n;
+    cin >> n;
+    vector<long long> combinations(n + 1, 0);
+    combinations[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= 6; j++) {
+            if (i - j >= 0) {
+                combinations[i] = (combinations[i] + combinations[i - j]);
+            }
+        }
+    }
+    cout << combinations[n] << endl;
+    return 0;
 }
