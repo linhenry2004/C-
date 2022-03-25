dd3cb599275282537cd721027a70fae1deea3825
Henry Lin <linhenry2004@gmail.com>
2022-03-26 00:10:25 +0800

    POTD

--- POTD/Dice_Combinations.cpp
+++ POTD/Dice_Combinations.cpp
@@ -11,7 +11,7 @@ int main () {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= 6; j++) {
             if (i - j >= 0) {
-                combinations[i] = (combinations[i] + combinations[i - j]);
+                combinations[i] = (combinations[i] + combinations[i - j]) % mod;
             }
         }
     }
