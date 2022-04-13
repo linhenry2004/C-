36081de8ce5ca40e6723b6a796c1ddc5fba7be42
Henry Lin <linhenry2004@gmail.com>
2022-04-13 20:36:07 +0800

    Lagrange's Four-Square Theorem

---
+++ Practice/GetMinSquares.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool isSquare(int x) {
+    int sqRoot = sqrt(x);
+    return (sqRoot * sqRoot == x);
+}
+
+int cntSquares(int n) {
+    if (isSquare(n)) {
+        return 1;
+    }
+    for (int i = 1; i <= (int)sqrt(n); i++) {
+        if (isSquare(n - (i * i))) {
+            return 2;
+        }
+    }
+    while (n % 4 == 0) {
+        n >>= 2;
+    }
+    if (n % 8 == 7) {
+        return 4;
+    }
+    return 3;
+}
+
+int main () {
+    int n;
+    cin >> n;
+    cout << cntSquares(n) << endl;
+    return 0;
+}
