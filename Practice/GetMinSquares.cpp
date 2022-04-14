469c7cb9801602c9f5520e45b4f3c0c644dd42c0
Henry Lin <linhenry2004@gmail.com>
2022-04-14 23:05:29 +0800

    POTD

--- Practice/GetMinSquares.cpp
+++ Practice/GetMinSquares.cpp
@@ -11,13 +11,13 @@ int cntSquares(int n) {
     if (isSquare(n)) {
         return 1;
     }
-    for (int i = 1; i <= (int)sqrt(n); i++) {
+    for (int i = 1; i <= (int)sqrt(n) / 2; i++) {
         if (isSquare(n - (i * i))) {
             return 2;
         }
     }
     while (n % 4 == 0) {
-        n >>= 2;
+        n /= 4;
     }
     if (n % 8 == 7) {
         return 4;
